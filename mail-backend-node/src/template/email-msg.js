1c9efed23a18dac1b868b41e06946f3bff7ed330
SIJULY <sijuly@outlook.com>
2026-08-08 08:10:19 +0800

    Fix Telegram notification issue due to unescaped HTML characters

--- mail-backend-node/src/template/email-msg.js
+++ mail-backend-node/src/template/email-msg.js
@@ -1,29 +1,36 @@
+function escapeHtml(text = '') {
+	return (text || '')
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;');
+}
+
 export default function emailMsgTemplate(email, tgMsgTo, tgMsgFrom) {
 
-	let template = `<b>${email.subject}</b>`
+	let template = `<b>${escapeHtml(email.subject)}</b>`
 
 		if (tgMsgFrom === 'only-name') {
 			template += `
 
-发件人：${email.name}`
+发件人：${escapeHtml(email.name)}`
 		}
 
 		if (tgMsgFrom === 'show') {
 			template += `
 
-发件人：${email.name}  &lt;${email.sendEmail}&gt;`
+发件人：${escapeHtml(email.name)}  &lt;${escapeHtml(email.sendEmail)}&gt;`
 		}
 
 		if(tgMsgTo === 'show' && tgMsgFrom === 'hide') {
 			template += `
 
-收件人：\u200B${email.toEmail}`
+收件人：\u200B${escapeHtml(email.toEmail)}`
 			return template
 		}
 
 	if(tgMsgTo === 'show') {
 		template += `
-收件人：\u200B${email.toEmail}`
+收件人：\u200B${escapeHtml(email.toEmail)}`
 	}
 
 		return template;
